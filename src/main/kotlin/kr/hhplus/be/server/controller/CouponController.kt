c7bdb5828d54211eef4aec49ec0ea5d5f13d0664
nmin11 <nmin1124@gmail.com>
2025-04-04 01:41:19 +0900

    docs: 쿠폰 발급 Swagger API 코드 작성

---
+++ src/main/kotlin/kr/hhplus/be/server/controller/CouponController.kt
@@ -0,0 +1,22 @@
+package kr.hhplus.be.server.controller
+
+import kr.hhplus.be.server.api.CouponApi
+import kr.hhplus.be.server.dto.CouponIssueRequest
+import kr.hhplus.be.server.dto.CouponIssueResponse
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.RestController
+import java.time.Instant
+
+@RestController
+class CouponController : CouponApi {
+    override fun issue(id: Long, request: CouponIssueRequest): ResponseEntity<CouponIssueResponse> {
+        val response = CouponIssueResponse(
+            couponId = id,
+            customerId = request.customerId,
+            status = "ISSUED",
+            issuedAt = Instant.now().toString()
+        )
+
+        return ResponseEntity.status(201).body(response)
+    }
+}
