a98572fa9a4332912a6091493b21658eb279dac4
nmin11 <nmin1124@gmail.com>
2025-04-08 04:19:22 +0900

    test: CustomerService 단위 테스트 작성

---
+++ src/test/java/kr/hhplus/be/server/domain/customer/CustomerServiceTest.kt
@@ -0,0 +1,42 @@
+package kr.hhplus.be.server.domain.customer
+
+import io.mockk.every
+import io.mockk.mockk
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class CustomerServiceTest {
+    private val customerRepository = mockk<CustomerRepository>()
+    private val customerService = CustomerService(customerRepository)
+
+    @Test
+    @DisplayName("존재하는 고객에 대한 조회 성공")
+    fun validateCustomerExistence_success() {
+        // given
+        val customerId = 1L
+        every { customerRepository.existsById(customerId) } returns true
+
+        // when & then
+        customerService.validateCustomerExistence(customerId)
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 고객 조회 시 예외 발생")
+    fun validateCustomerExistence_customerNotExist() {
+        // given
+        val customerId = 999L
+        every { customerRepository.existsById(customerId) } returns false
+
+        // when
+        val exception = assertThrows<IllegalArgumentException> {
+            customerService.validateCustomerExistence(customerId)
+        }
+
+        // then
+        assertThat(exception)
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessage("사용자를 찾을 수 없습니다.")
+    }
+}
