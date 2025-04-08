a02a677d39c3faeeb7d497ba887877b0dbb46b86
nmin11 <nmin1124@gmail.com>
2025-04-09 00:57:26 +0900

    test: StatisticService 단위 테스트 작성

---
+++ src/test/java/kr/hhplus/be/server/domain/product/StatisticServiceTest.kt
@@ -0,0 +1,38 @@
+package kr.hhplus.be.server.domain.product
+
+import io.mockk.every
+import io.mockk.mockk
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import java.time.LocalDate
+import java.time.LocalDateTime
+
+class StatisticServiceTest {
+    private val statisticRepository = mockk<StatisticRepository>()
+    private val statisticService = StatisticService(statisticRepository)
+
+    @Test
+    @DisplayName("지정된 날짜 이후의 판매량 상위 5개 상품 통계 반환")
+    fun returnTop5StatisticsSinceDate() {
+        // given
+        val since = LocalDate.now().minusDays(3)
+        val statistics = listOf(
+            Statistic(productId = 1L, salesCount = 12).apply {
+                id = 1L
+                soldAt = LocalDateTime.now().minusDays(1)
+            },
+            Statistic(productId = 2L, salesCount = 9).apply {
+                id = 2L
+                soldAt = LocalDateTime.now().minusDays(2)
+            }
+        )
+        every { statisticRepository.findTop5BySoldAtAfterOrderBySalesCountDesc(since) } returns statistics
+
+        // when
+        val result = statisticService.getTop5PopularProductStatistics(since)
+
+        // then
+        assertThat(result).isEqualTo(statistics)
+    }
+}
