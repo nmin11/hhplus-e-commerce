16aab24fb7212e6c5f5ec519a677dcd6ea2678e8
nmin11 <nmin1124@gmail.com>
2025-04-15 14:01:59 +0900

    chore: Customer 객체에서 사용되지 않는 필드들 제거

--- src/main/kotlin/kr/hhplus/be/server/domain/customer/Customer.kt
+++ src/main/kotlin/kr/hhplus/be/server/domain/customer/Customer.kt
@@ -1,10 +1,5 @@
 package kr.hhplus.be.server.domain.customer
 
-import kr.hhplus.be.server.domain.balance.Balance
-import kr.hhplus.be.server.domain.balance.BalanceHistory
-import kr.hhplus.be.server.domain.coupon.CustomerCoupon
-import kr.hhplus.be.server.domain.order.Order
-import kr.hhplus.be.server.domain.payment.Payment
 import java.time.LocalDateTime
 
 class Customer private constructor(
@@ -13,11 +8,6 @@ class Customer private constructor(
     val id: Long = 0L
     val createdAt: LocalDateTime = LocalDateTime.now()
     var updatedAt: LocalDateTime = LocalDateTime.now()
-    var balance: Balance? = null
-    val balanceHistories: MutableList<BalanceHistory> = mutableListOf()
-    val customerCoupons: MutableList<CustomerCoupon> = mutableListOf()
-    val orders: MutableList<Order> = mutableListOf()
-    val payments: MutableList<Payment> = mutableListOf()
 
     companion object {
         fun create(username: String): Customer {
