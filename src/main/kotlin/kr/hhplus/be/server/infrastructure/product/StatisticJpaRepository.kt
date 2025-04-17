a7f46824efa833e4672393a2f841d7e7c14a2e51
nmin11 <nmin1124@gmail.com>
2025-04-17 21:31:34 +0900

    chore: 사용되지 않는 통계 Repository 함수 제거

--- src/main/kotlin/kr/hhplus/be/server/infrastructure/product/StatisticJpaRepository.kt
+++ src/main/kotlin/kr/hhplus/be/server/infrastructure/product/StatisticJpaRepository.kt
@@ -2,8 +2,5 @@ package kr.hhplus.be.server.infrastructure.product
 
 import kr.hhplus.be.server.domain.product.Statistic
 import org.springframework.data.jpa.repository.JpaRepository
-import java.time.LocalDateTime
 
-interface StatisticJpaRepository : JpaRepository<Statistic, Long> {
-    fun findTop5BySoldAtAfterOrderBySalesCountDesc(since: LocalDateTime): List<Statistic>
-}
+interface StatisticJpaRepository : JpaRepository<Statistic, Long>
