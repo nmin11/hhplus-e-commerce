03b3711dfd64bfa0bec0fba6dc90f36e3cd314bc
nmin11 <nmin1124@gmail.com>
2025-04-17 15:50:34 +0900

    test: 쿠폰 중복 발급 동시성 테스트
    - 한 사용자가 한 쿠폰을 여러 번 발급 받는 경우
    - 여러 사용자가 한 쿠폰을 동시에 발급 받는 경우

--- src/main/resources/database/schema.sql
+++ src/main/resources/database/schema.sql
@@ -87,9 +87,7 @@ CREATE TABLE IF NOT EXISTS coupon (
     started_at TIMESTAMP NOT NULL,
     expired_at TIMESTAMP NOT NULL,
     created_at TIMESTAMP,
-    updated_at TIMESTAMP,
-
-    UNIQUE KEY uq_coupon_name (name)
+    updated_at TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS customer_coupon (
@@ -100,7 +98,7 @@ CREATE TABLE IF NOT EXISTS customer_coupon (
     issued_at TIMESTAMP,
     updated_at TIMESTAMP,
 
-    UNIQUE KEY uq_customer_coupon_customer_id_coupon_id (customer_id, coupon_id),
+    -- UNIQUE KEY uq_customer_coupon_customer_id_coupon_id (customer_id, coupon_id), <- 동일한 사용자의 쿠폰 중복 발급 동시성 테스트를 통과하려면 해당 주석을 풀어야 함
     INDEX idx_customer_coupon_customer_id (customer_id),
     INDEX idx_customer_coupon_coupon_id (coupon_id)
     -- FOREIGN KEY (customer_id) REFERENCES customer(id),
