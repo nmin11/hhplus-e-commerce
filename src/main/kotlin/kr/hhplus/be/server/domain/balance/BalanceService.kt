d2e03f91a789be377ea558e81e8059c91839c769
nmin11 <nmin1124@gmail.com>
2025-04-17 15:30:55 +0900

    test: 잔액 차감 동시성 테스트

--- src/main/kotlin/kr/hhplus/be/server/domain/balance/BalanceService.kt
+++ src/main/kotlin/kr/hhplus/be/server/domain/balance/BalanceService.kt
@@ -1,6 +1,7 @@
 package kr.hhplus.be.server.domain.balance
 
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class BalanceService(
@@ -17,6 +18,7 @@ class BalanceService(
         return balanceRepository.save(balance)
     }
 
+    // @Transactional <- 잔액 차감 동시성 테스트 해결을 하기 위해 설정해야 하는 어노테이션
     fun deduct(customerId: Long, amount: Int): Balance {
         val balance = getByCustomerId(customerId)
         balance.deduct(amount)
