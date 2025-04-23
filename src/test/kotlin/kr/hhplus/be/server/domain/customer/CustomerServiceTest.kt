4c85e3f1a4f55a230ceed299ed76c1632ba01a2e
nmin11 <nmin1124@gmail.com>
2025-04-23 23:16:40 +0900

    chore: Customer 관련 예외 클래스들 정의

--- src/test/kotlin/kr/hhplus/be/server/domain/customer/CustomerServiceTest.kt
+++ src/test/kotlin/kr/hhplus/be/server/domain/customer/CustomerServiceTest.kt
@@ -2,6 +2,7 @@ package kr.hhplus.be.server.domain.customer
 
 import io.mockk.every
 import io.mockk.mockk
+import kr.hhplus.be.server.support.exception.customer.CustomerNotFoundException
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
@@ -30,13 +31,13 @@ class CustomerServiceTest {
         every { customerRepository.existsById(customerId) } returns false
 
         // when
-        val exception = assertThrows<IllegalArgumentException> {
+        val exception = assertThrows<CustomerNotFoundException> {
             customerService.validateCustomerExistence(customerId)
         }
 
         // then
         assertThat(exception)
-            .isInstanceOf(IllegalArgumentException::class.java)
+            .isInstanceOf(CustomerNotFoundException::class.java)
             .hasMessage("사용자를 찾을 수 없습니다.")
     }
 }
