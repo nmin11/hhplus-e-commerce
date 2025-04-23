dceda6644da6942763e5748bff6225275a785eb9
nmin11 <nmin1124@gmail.com>
2025-04-24 02:27:41 +0900

    chore: 데이터 전송 플랫폼 사용 시 URL 정보를 은닉하도록 변경

--- src/main/kotlin/kr/hhplus/be/server/infrastructure/dataplatform/MockApiDataPlatformSender.kt
+++ src/main/kotlin/kr/hhplus/be/server/infrastructure/dataplatform/MockApiDataPlatformSender.kt
@@ -3,16 +3,19 @@ package kr.hhplus.be.server.infrastructure.dataplatform
 import kr.hhplus.be.server.application.dataplatform.DataPlatformCommand
 import kr.hhplus.be.server.application.dataplatform.DataPlatformSender
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.MediaType
 import org.springframework.stereotype.Component
 import org.springframework.web.client.RestClient
 
 @Component
 class MockApiDataPlatformSender(
-    private val restClient: RestClient
+    private val restClient: RestClient,
+
+    @Value("\${external.data-platform.url}")
+    private val apiUrl: String
 ) : DataPlatformSender {
     private val log = LoggerFactory.getLogger(MockApiDataPlatformSender::class.java)
-    private val apiUrl = "https://67f65cb942d6c71cca61b523.mockapi.io/order"
 
     override fun send(command: DataPlatformCommand.Order) {
         try {
