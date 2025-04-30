9e03f16a19b7ef248a9e26020515f641da101198
nmin11 <nmin1124@gmail.com>
2025-04-30 16:42:58 +0900

    chore: Redis 관련 testcontainer 설정

--- src/test/kotlin/kr/hhplus/be/server/TestcontainersConfiguration.kt
+++ src/test/kotlin/kr/hhplus/be/server/TestcontainersConfiguration.kt
@@ -2,29 +2,46 @@ package kr.hhplus.be.server
 
 import jakarta.annotation.PreDestroy
 import org.springframework.context.annotation.Configuration
+import org.springframework.test.context.DynamicPropertyRegistry
+import org.springframework.test.context.DynamicPropertySource
+import org.testcontainers.containers.GenericContainer
 import org.testcontainers.containers.MySQLContainer
 import org.testcontainers.utility.DockerImageName
 
 @Configuration
 class TestcontainersConfiguration {
-    @PreDestroy
-    fun preDestroy() {
-        if (mySqlContainer.isRunning) mySqlContainer.stop()
-    }
-
     companion object {
-        val mySqlContainer: MySQLContainer<*> = MySQLContainer(DockerImageName.parse("mysql:8.0"))
+        private val mysqlContainer: MySQLContainer<*> = MySQLContainer(DockerImageName.parse("mysql:8.0"))
             .withDatabaseName("hhplus")
             .withUsername("test")
             .withPassword("test")
-            .apply {
-                start()
-            }
+
+        private val redisContainer = GenericContainer(DockerImageName.parse("redis:latest"))
+            .withExposedPorts(6379)
+            .withCommand("redis-server --requirepass root")
 
         init {
-            System.setProperty("spring.datasource.url", mySqlContainer.getJdbcUrl() + "?characterEncoding=UTF-8&serverTimezone=UTC")
-            System.setProperty("spring.datasource.username", mySqlContainer.username)
-            System.setProperty("spring.datasource.password", mySqlContainer.password)
+            mysqlContainer.start()
+            redisContainer.start()
         }
+
+        @JvmStatic
+        @DynamicPropertySource
+        fun registerDynamicProperties(registry: DynamicPropertyRegistry) {
+            // MySQL
+            registry.add("spring.datasource.url") { mysqlContainer.jdbcUrl + "?characterEncoding=UTF-8&serverTimezone=UTC" }
+            registry.add("spring.datasource.username") { mysqlContainer.username }
+            registry.add("spring.datasource.password") { mysqlContainer.password }
+
+            // Redis
+            registry.add("spring.redis.host") { redisContainer.host }
+            registry.add("spring.redis.port") { redisContainer.getMappedPort(6379).toString() }
+        }
+    }
+
+    @PreDestroy
+    fun preDestroy() {
+        if (mysqlContainer.isRunning) mysqlContainer.stop()
+        if (redisContainer.isRunning) redisContainer.stop()
     }
 }
