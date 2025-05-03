e03f68ec4a37f54f0a9e42b915ed046e2e993bbf
nmin11 <nmin1124@gmail.com>
2025-05-03 21:43:16 +0900

    test: 인기 상품 조회 기능에 캐시 관련 통합 테스트 케이스 추가 작성

--- src/test/kotlin/kr/hhplus/be/server/application/product/ProductFacadeIntegrationTest.kt
+++ src/test/kotlin/kr/hhplus/be/server/application/product/ProductFacadeIntegrationTest.kt
@@ -1,5 +1,6 @@
 package kr.hhplus.be.server.application.product
 
+import com.github.benmanes.caffeine.cache.Cache
 import kr.hhplus.be.server.domain.product.*
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeAll
@@ -9,7 +10,6 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.data.redis.core.StringRedisTemplate
 import org.springframework.test.context.ActiveProfiles
 
@@ -21,11 +21,16 @@ class ProductFacadeIntegrationTest @Autowired constructor(
     private val productRepository: ProductRepository,
     private val productOptionRepository: ProductOptionRepository,
     private val statisticRepository: StatisticRepository,
-    private val stringRedisTemplate: StringRedisTemplate
+    private val stringRedisTemplate: StringRedisTemplate,
+    private val caffeineCache: Cache<String, String>
 ) {
     private lateinit var product: Product
     private lateinit var option1: ProductOption
     private lateinit var option2: ProductOption
+
+    companion object {
+        private const val CACHE_KEY = "product:popular:3d"
+    }
 
     @BeforeAll
     fun setup() {
@@ -41,7 +46,8 @@ class ProductFacadeIntegrationTest @Autowired constructor(
 
     @BeforeEach
     fun clearCache() {
-        stringRedisTemplate.delete("product:popular:3d")
+        caffeineCache.invalidate(CACHE_KEY)
+        stringRedisTemplate.delete(CACHE_KEY)
     }
 
     @Test
@@ -69,13 +75,52 @@ class ProductFacadeIntegrationTest @Autowired constructor(
         )
         stats.forEach { statisticRepository.save(it) }
 
-        // when
         val condition = ProductCriteria.PeriodCondition(days = 3)
+
+        // when
         val result = productFacade.getPopularProducts(condition)
 
         // then
-        assertThat(result).hasSizeGreaterThanOrEqualTo(2)
         assertThat(result.first().productId).isIn(product.id)
         assertThat(result.first().totalSales).isEqualTo(Int.MAX_VALUE)
+    }
+
+    @Test
+    @DisplayName("인기 상품 조회 시 캐시 미스 → Redis 및 In-Memory 캐시에 저장")
+    fun getPopularProducts_shouldCacheWhenMiss() {
+        // given
+        val product3 = Product.create("비니", basePrice = 7000)
+        productRepository.save(product3)
+
+        val statistic = Statistic.create(product3, 123)
+        statisticRepository.save(statistic)
+
+        // when
+        val result = productFacade.getPopularProducts(ProductCriteria.PeriodCondition(3))
+
+        // then
+        assertThat(result).anyMatch { it.name == "비니" }
+
+        // redis 캐시 확인
+        val redisCache = stringRedisTemplate.opsForValue().get("product:popular:3d")
+        assertThat(redisCache).isNotNull()
+        assertThat(redisCache).contains("비니")
+    }
+
+    @Test
+    @DisplayName("인기 상품 조회 시 캐시 히트 → DB에 접근하지 않고 캐시 데이터 반환")
+    fun getPopularProducts_shouldUseCacheWhenAvailable() {
+        // given
+        val cachedJson = """
+            [{"id": 2021, "name": "코트", "basePrice": 39000, "totalSales": 100}]
+        """.trimIndent()
+        stringRedisTemplate.opsForValue().set("product:popular:3d", cachedJson)
+
+        // when
+        val result = productFacade.getPopularProducts(ProductCriteria.PeriodCondition(3))
+
+        // then
+        assertThat(result[0].name).isEqualTo("코트")
+        assertThat(result[0].totalSales).isEqualTo(100)
     }
 }
